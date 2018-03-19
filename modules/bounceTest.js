a9e74cd5f33e9328e059f58c2ededc0d56f7a68c
leotseil <37208031+leotseil@users.noreply.github.com>
2018-03-19 12:43:45 +0100

    Animasjonstest

---
+++ modules/bounceTest.js
@@ -0,0 +1,162 @@
+"use strict"
+
+var canvas = document.createElement("canvas");
+canvas.style.border = "2px solid black";
+canvas.width = 600;
+canvas.height = 400;
+
+var CANVAS_WIDTH = canvas.width;
+var CANVAS_HEIGHT = canvas.height;
+
+document.body.appendChild(canvas);
+var ctx = canvas.getContext("2d");
+
+// Handle inputs ------------------------------------------------------------
+var keydown = {};
+var keypress = {};
+var mouse = {x: 0, y: 0};
+var mousepress = {};
+var mousedown = {};
+
+document.addEventListener("keydown", function(event) {
+    keydown[event.code] = true;
+
+    if(!event.repeat) {
+        keypress[event.code] = true;
+    } else {
+        keypress[event.code] = false;
+    }
+});
+
+document.addEventListener("keyup", function(event) {
+    keydown[event.code] = false;
+    keypress[event.code] = false;
+});
+
+document.addEventListener('mousemove', function(event) {
+    var rect = canvas.getBoundingClientRect();
+    mouse.x = event.clientX - rect.left;
+    mouse.y = event.clientY - rect.top;
+});
+
+function keyPressed(str) {
+    var result = keypress[str];
+    if(result) {
+        keypress[str] = false;
+        return true;
+    } 
+    return false;
+}
+
+function keyDown(str) {
+    if(keydown[str]) return true;
+    else return false;
+}
+
+function mouseDown(str) {
+
+}
+
+function mousePressed(str) {
+    
+}
+
+
+// 2D Primitives ------------------------------------------------------------
+function rect(x,y,w,h) {
+    ctx.fillRect(x,y,w,h);
+}
+
+function line(x0,y0, x1,y1) {
+
+    ctx.beginPath();
+    ctx.moveTo(x0, y0);
+    ctx.lineTo(x1,y1);
+    ctx.stroke();
+}
+
+function triangle(x0,y0, x1,y1, x2,y2) {
+
+}
+
+function quad(x0,y0, x1,y1, x2,y2, x3,y3) {
+
+}
+
+// Sprites ------------------------------------------------------------------
+function Sprite(filepath) {
+    this.x = 0;
+    this.y = 0;
+    this.img = new Image();
+    this.img.src = filepath;
+}
+
+Sprite.prototype.draw = function() {
+    ctx.drawImage(this.img, this.x, this.y);
+};
+
+Sprite.prototype.collide = function(sprite) {
+    return !(this.x > sprite.x + sprite.img.width ||
+             this.x + this.img.width < sprite.x ||
+             this.y > sprite.y + sprite.img.height ||
+             this.y + this.img.height < sprite.y);
+}
+
+//---------------------------------------------------------------------------
+var x = 0;
+var y = 100;
+
+var vy = -350;
+var vx = 150;
+
+var ay = 800;
+
+
+//---------------------------------------------------------------------------
+
+function update(dt) {
+    vy += ay*dt;
+
+    x += vx*dt;
+    y += vy*dt;
+
+    if(y >= CANVAS_HEIGHT - 25) {
+        y = CANVAS_HEIGHT - 25;
+        vy = -vy*0.6;
+    }
+
+    if(x >= CANVAS_WIDTH) {
+        x = 0;
+        y = 100;
+        vy = -350;
+    }
+}
+
+function render() {
+    ctx.fillRect(x,y,25,25);
+}
+
+if(typeof(update) == "function" && typeof(render) == "function") {
+    var oldtime = window.performance.now()
+    var dt;
+    var step = 1/60;
+    var acc = 0;
+    function mainLoop(){
+        var newtime = window.performance.now();
+        dt = Math.min(1, (newtime - oldtime) / 1000);
+
+        acc += dt;
+
+        while(acc >= step) {
+            acc -= step;
+            update(step);
+        }
+        
+        ctx.clearRect(0,0,CANVAS_WIDTH, CANVAS_HEIGHT);
+        render();
+
+        oldtime = newtime;
+        window.requestAnimationFrame(mainLoop);
+    };
+    window.requestAnimationFrame(mainLoop);
+}  
