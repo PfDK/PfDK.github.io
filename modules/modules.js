dad47d3c71addf2bd0ec88d6e16fee01e4da9003
etsikt <erlend.thune@gmail.com>
2018-03-20 17:45:21 +0100

    Fix module name.

--- modules/modules.js
+++ modules/modules.js
@@ -1,4 +1,4 @@
 $.getScript("https://pfdk.github.io/modules/youtube.js");
 $.getScript("https://pfdk.github.io/modules/bounceTest.js");
-$.getScript("https://pfdk.github.io/modules/ordskymodul.js");
+$.getScript("https://pfdk.github.io/modules/ordsky.js");
 //$.getScript("https://pfdk.github.io/modules/yourfile.js");
