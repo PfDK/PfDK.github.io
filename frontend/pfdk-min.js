bd06199a1f12c16ac4f63388d7876a36df12d85f
etsikt <erlend.thune@gmail.com>
2018-05-31 09:55:18 +0200

    New boxes.

--- frontend/pfdk-min.js
+++ frontend/pfdk-min.js
@@ -3434,10 +3434,10 @@ function uobAddComponents() {
 		// Boxes
 		//
 		// Create boxes from all tables with the codes: uob-tip, uob-info, uob-warning,
-		// uob-header,pfdk-info, pfdk-important and uob-question.
+		// uob-header,pfdk-info, pfdk-important, pfdk-tid, pfdk-verktoy and uob-question.
 		// --------------------------------------------------------------------------------
 
-		aBoxTags = ["uob-tip", "uob-read", "uob-info", "uob-warning", "uob-header", "uob-question", "uob-quote", "uob-box", "pfdk-info", "pfdk-important"];
+		aBoxTags = ["uob-tip", "uob-read", "uob-info", "uob-warning", "uob-header", "uob-question", "uob-quote", "uob-box", "pfdk-info", "pfdk-important", "pfdk-tid", "pfdk-verktoy"];
 
 		for (var i = 0; i < aBoxTags.length; i++) {
 			var strTag = aBoxTags[i];
@@ -5786,6 +5786,10 @@ this.mmooc.api = function() {
             }
         },
 
+        getLocale : function() {
+            return this._env.LOCALE;
+        },
+        
         getRoles : function() {
             return this._env.current_user_roles;
         },
@@ -9040,57 +9044,125 @@ this.mmooc.settings = {
 this.mmooc=this.mmooc||{};
 
 if (typeof this.mmooc.i18n === 'undefined') {
-	this.mmooc.i18n = {
-	    'DropCourseDialogText' : 'Trykk OK for å melde deg av kurset ',
-	    'JoinCourseDialogText' : 'Du kan melde deg på kurset igjen senere om du vil ',
-	    'DropCourse' : 'Meld deg av kurset',
-	    'CreateAccountTitle' : 'Har du ikke konto?',
-	    'CreateAccountSubtitle' : 'Klikk her for å lage en',
-		'Course' : 'Kurs',
-		'CourseDefinite' : 'Kurset',
-		'CoursePlural' : 'Kurs',
-		'CourseProgressionTitle' : 'Din kursprogresjon:',
-		'GoToCourse' : 'Gå til kursside',
-		'GoToModule' : 'Gå til modul',
-		'BackToCoursePage' : 'Tilbake til kursforsiden',
-		'AddACourse' : 'Legg til et kurs',
-		'Module' : 'Modul',
-		'ModulePlural' : 'Moduler',
-		'CourseModules' : 'Kursmoduler',
-		'Assignment' : 'Oppgave',
-		'Discussion' : 'Diskusjon',
-		'Quiz' : 'Prøve',
-		'Page' : 'Innholdsside',
-		'ExternalUrl' : 'Ekstern lenke',
-		'ExternalTool' : 'Eksternt verktøy',
-		'File' : 'Fil',
-		'Announcement' : 'Kunngjøring',
-		'DiscussionTopic': 'Diskusjon',
-		'Conversation': 'Innboksmelding',
-		'Message': 'Oppgavevarsel',
-		'Submission': 'Innlevering',
-		'AssessmentRequest': 'Vurderingsforespørsel',
-		'Conference': 'Conference',
-		'Collaboration': 'Collaboration',
-		'LinkBack': 'Tilbake til forrige side',
-		'Badgesafe': 'Utmerkelser',
-		'PeerReview' : 'Hverandrevurdering',
-		'PeerReviewer' : 'Fagfellevurdering',
-		'Delivery' : 'Innlevering',
-		'DetailsAboutYourDelivery' : 'Detaljer om din innlevering',
-		'DetailsAboutDelivery' : 'Detaljer om innlevering',
-		'SubmissionIsNotAssessed' : 'Oppgaven er ikke vurdert',
-		'SubmissionIsAssessedByOne' : 'Vurderingen er klar',
-		'SubmissionIsAssessedByAll' : 'Alle vurderinger er klare',
-		'SubmissionAssessmentsAreReady' : 'vurderinger er klare',
-		'ThisIsGroup' : 'Dette er gruppe',
-		'eventsAndDeadlinesTitle' : 'Viktige datoer',
-		'WeHaveAQuestionToTeacherInTheDiscussion' : 'Vi har et spørsmål til veileder i diskusjonen',
-		'NoTeacherFeedbackLink' : 'No teacher_feedback link. Does the help menu have a link to send feedback to the teacher?',
-		'NoEnrollments' : 'Velkommen til vår kursplattform. Du er ikke påmeldt noen kurs enda. Klikk på knappen nedenfor for å se tilgjengelige kurs.',
-		'OutOf' : 'av',
-		'Months' : ["januar", "februar", "mars", "april", "mai", "juni", "juli", "august", "september", "Oktober", "november", "december"]
-	};
+    if(mmooc.api.getLocale() == "nn")
+    {
+        this.mmooc.i18n = {
+            'DropCourseDialogText' : 'Trykk OK for å melde deg av kurset ', 
+            'JoinCourseDialogText' : 'Du kan melde deg på kurset igjen seinare om du vil ', 
+            'DropCourse' : 'Meld deg av kurset', 
+            'CreateAccountTitle' : 'Har du ikkje konto?', 
+            'CreateAccountSubtitle' : 'Klikk her for å lage ein', 
+             'Course' : 'Kurs', 
+             'CourseDefinite' : 'Kurset', 
+             'CoursePlural' : 'Kurs', 
+             'CourseProgressionTitle' : 'Din kursprogresjon:', 
+             'GoToCourse' : 'Gå til kursside', 
+             'GoToModule' : 'Gå til modul', 
+             'BackToCoursePage' : 'Tilbake til kursforsida', 
+             'AddACourse' : 'Legg til eit kurs', 
+             'Module' : 'Modul', 
+             'ModulePlural' : 'Modular', 
+             'CourseModules' : 'Kursmodular', 
+             'Assignment' : 'Oppgåve', 
+             'Discussion' : 'Diskusjon', 
+             'Quiz' : 'Prøve', 
+             'Page' : 'Innhaldsside', 
+             'ExternalUrl' : 'Ekstern lenke', 
+             'ExternalTool' : 'Eksternt verktøy', 
+             'File' : 'Fil', 
+             'Announcement' : 'Kunngjering', 
+             'DiscussionTopic': 'Diskusjon', 
+             'Conversation': 'Innboksmelding', 
+             'Message': 'Oppgåvevarsel', 
+             'Submission': 'Innlevering', 
+             'AssessmentRequest': 'Vurderingsførespurnad', 
+             'Conference': 'Conference', 
+             'Collaboration': 'Collaboration', 
+             'LinkBack': 'Tilbake til førre side', 
+             'Badgesafe': 'Utmerkingar', 
+             'PeerReview' : 'Kvarandrevurdering', 
+             'PeerReviewer' : 'Fagfellevurdering', 
+             'Delivery' : 'Innlevering', 
+             'DetailsAboutYourDelivery' : 'Detaljar om innleveringa di', 
+             'DetailsAboutDelivery' : 'Detaljar om innlevering', 
+             'SubmissionIsNotAssessed' : 'Oppgåva er ikkje vurdert', 
+             'SubmissionIsAssessedByOne' : 'Vurderinga er klar', 
+             'SubmissionIsAssessedByAll' : 'Alle vurderingar er klare', 
+             'SubmissionAssessmentsAreReady' : 'vurderingar er klare', 
+             'ThisIsGroup' : 'Dette er gruppe', 
+             'eventsAndDeadlinesTitle' : 'Viktige datoar', 
+             'WeHaveAQuestionToTeacherInTheDiscussion' : 'Vi har eit spørsmål til rettleiar i diskusjonen', 
+             'NoTeacherFeedbackLink' : 'No teacher_feedback link. Does the help menu have a link to send feedback to the teacher?', 
+             'NoEnrollments' : 'Velkommen til vår kursplattform. Du er ikkje påmeldt nokon kurs endå. Klikk på knappen nedanfor for å sjå tilgjengelege kurs.', 
+             'OutOf' : 'av', 
+             'Months' : ["januar", 
+             "februar", 
+             "mars", 
+             "april", 
+             "mai", 
+             "juni", 
+             "juli", 
+             "august", 
+             "september", 
+             "oktober", 
+             "november", 
+             "desember"] 
+            }
+    }
+    else
+    {
+        this.mmooc.i18n = {
+            'DropCourseDialogText' : 'Trykk OK for å melde deg av kurset ',
+            'JoinCourseDialogText' : 'Du kan melde deg på kurset igjen senere om du vil ',
+            'DropCourse' : 'Meld deg av kurset',
+            'CreateAccountTitle' : 'Har du ikke konto?',
+            'CreateAccountSubtitle' : 'Klikk her for å lage en',
+            'Course' : 'Kurs',
+            'CourseDefinite' : 'Kurset',
+            'CoursePlural' : 'Kurs',
+            'CourseProgressionTitle' : 'Din kursprogresjon:',
+            'GoToCourse' : 'Gå til kursside',
+            'GoToModule' : 'Gå til modul',
+            'BackToCoursePage' : 'Tilbake til kursforsiden',
+            'AddACourse' : 'Legg til et kurs',
+            'Module' : 'Modul',
+            'ModulePlural' : 'Moduler',
+            'CourseModules' : 'Kursmoduler',
+            'Assignment' : 'Oppgave',
+            'Discussion' : 'Diskusjon',
+            'Quiz' : 'Prøve',
+            'Page' : 'Innholdsside',
+            'ExternalUrl' : 'Ekstern lenke',
+            'ExternalTool' : 'Eksternt verktøy',
+            'File' : 'Fil',
+            'Announcement' : 'Kunngjøring',
+            'DiscussionTopic': 'Diskusjon',
+            'Conversation': 'Innboksmelding',
+            'Message': 'Oppgavevarsel',
+            'Submission': 'Innlevering',
+            'AssessmentRequest': 'Vurderingsforespørsel',
+            'Conference': 'Conference',
+            'Collaboration': 'Collaboration',
+            'LinkBack': 'Tilbake til forrige side',
+            'Badgesafe': 'Utmerkelser',
+            'PeerReview' : 'Hverandrevurdering',
+            'PeerReviewer' : 'Fagfellevurdering',
+            'Delivery' : 'Innlevering',
+            'DetailsAboutYourDelivery' : 'Detaljer om din innlevering',
+            'DetailsAboutDelivery' : 'Detaljer om innlevering',
+            'SubmissionIsNotAssessed' : 'Oppgaven er ikke vurdert',
+            'SubmissionIsAssessedByOne' : 'Vurderingen er klar',
+            'SubmissionIsAssessedByAll' : 'Alle vurderinger er klare',
+            'SubmissionAssessmentsAreReady' : 'vurderinger er klare',
+            'ThisIsGroup' : 'Dette er gruppe',
+            'eventsAndDeadlinesTitle' : 'Viktige datoer',
+            'WeHaveAQuestionToTeacherInTheDiscussion' : 'Vi har et spørsmål til veileder i diskusjonen',
+            'NoTeacherFeedbackLink' : 'No teacher_feedback link. Does the help menu have a link to send feedback to the teacher?',
+            'NoEnrollments' : 'Velkommen til vår kursplattform. Du er ikke påmeldt noen kurs enda. Klikk på knappen nedenfor for å se tilgjengelige kurs.',
+            'OutOf' : 'av',
+            'Months' : ["januar", "februar", "mars", "april", "mai", "juni", "juli", "august", "september", "oktober", "november", "desember"]
+        };
+    }
 }
 
 jQuery(function($) {
