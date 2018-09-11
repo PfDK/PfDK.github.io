75de5cf478853239f3a2bc4c1e5a439000606a94
etsikt <erlend.thune@gmail.com>
2018-09-11 11:19:36 +0200

    Ikke vis tilbake til diskusjoner/gruppeside dersom man kom inn i diskusjonen via modulnavigasjon.

--- frontend/pfdk-min.js
+++ frontend/pfdk-min.js
@@ -9595,6 +9595,8 @@ this.mmooc.routes = function() {
                 if (route.paths != null) {
                     for (var j = 0; j < route.paths.length; j++) {
                         if (route.paths[j].test(path) && !route.isAlreadyHandled) {
+                            //20180911ETH Need to know if there is a query string to decide how to handle a discussion.
+                            route.hasQueryString = queryString.length;
                             route.isAlreadyHandled = true;
                             console.log("Handle route: " + route.paths[j]);
                             route.handler();
@@ -10438,20 +10440,25 @@ jQuery(function($) {
     /\/groups\/\d+\/discussion_topics\/\d+$/,
     /\/groups\/\d+\/discussion_topics\/new$/], function() {
         mmooc.menu.showDiscussionGroupMenu();
-        var courseId = mmooc.api.getCurrentCourseId();
         
-        //If courseId was found, it is a group discussion created by a teacher.
-        if(courseId) {
-            mmooc.menu.showBackButton("/courses/" + courseId + "/discussion_topics", "Tilbake til diskusjoner");                
-        }
-        else {
-            var groupId = mmooc.api.getCurrentGroupId();
-            if(null != groupId)
-            {
-                mmooc.api.getGroup(groupId,function(group) {
-                    var courseId = group.course_id;
-                    mmooc.menu.showBackButton("/groups/" + group.id + "/discussion_topics", "Tilbake til gruppeside");                
-                });
+        //20180911ETH Need to know if I got here from the discussion list or from the module
+        //            navigation.
+        if(!this.hasQueryString) {
+            var courseId = mmooc.api.getCurrentCourseId();
+        
+            //If courseId was found, it is a group discussion created by a teacher.
+            if(courseId) {
+                mmooc.menu.showBackButton("/courses/" + courseId + "/discussion_topics", "Tilbake til diskusjoner");                
+            }
+            else {
+                var groupId = mmooc.api.getCurrentGroupId();
+                if(null != groupId)
+                {
+                    mmooc.api.getGroup(groupId,function(group) {
+                        var courseId = group.course_id;
+                        mmooc.menu.showBackButton("/groups/" + group.id + "/discussion_topics", "Tilbake til gruppeside");                
+                    });
+                }
             }
         }
         
