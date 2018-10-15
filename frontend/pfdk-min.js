6557e725ae758cf2dd96bad886c91e176cb3920d
etsikt <erlend.thune@gmail.com>
2018-10-15 09:09:17 +0200

    Set disablePeerReviewButton to true.

--- frontend/pfdk-min.js
+++ frontend/pfdk-min.js
@@ -10260,7 +10260,7 @@ this.mmooc.settings = {
     'CanvaBadgeProtocolAndHost' : 'https://canvabadges-beta-iktsenteret.bibsys.no',
     'useCanvaBadge' : false,
     'defaultNumberOfReviews' : 2, // Default number of peer reviews per student in power function
-    'disablePeerReviewButton' : false,
+    'disablePeerReviewButton' : true,
     'removeGlobalGradesLink' : true,
     'removeGroupsLink' : true,
     'privacyPolicyLink' : 'http://matematikk-mooc.github.io/privacypolicy.html',
