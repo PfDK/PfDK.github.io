8f552dad4fcfd9f8649eb0d299ce1c5ea05dde9d
jamesstanforth <6414210+jamesstanforth@users.noreply.github.com>
2022-09-19 00:02:24 +0100

    Change input format

--- modules/wordcloud/wordcloud.js
+++ modules/wordcloud/wordcloud.js
@@ -68,7 +68,7 @@ function printWordcloud()
 	var fill = d3.scale.category20();
 
 	var layout = d3.layout.cloud()
-		.size([300, 300])
+		.size([500, 500])
 		.words(words)
 		.padding(5)
 		.rotate(function() { return 0; })
